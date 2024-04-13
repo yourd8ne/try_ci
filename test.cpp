4cff249ee3f5a7d00e675a41bf99d27db38ba8b8
Mikhail Safronov <45272028+yourd8ne@users.noreply.github.com>
2024-04-13 14:13:44 +0300

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include "mylib.hpp"
+
+int main(int argc, char** argv) {
+  int values[] = {4, 3, 11, 2};
+  int expected[] = {11, 10, 18, 9};
+
+  for(int i = 0; i < 4; i++) {
+    int result = process(values[i]);
+    if(result[i] != expected[i] {
+      std::cout << "Expected " << expected[i] << " but got " << result[i] << std::endl;
+      return 1;
+    }
+  }
+  return 0;
+}
